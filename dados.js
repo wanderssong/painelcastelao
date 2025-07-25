f2cc0b8f5e66851f675dfeb4f241f7c22955319a
sr_wandersson <161793765+wanderssong@users.noreply.github.com>
2025-07-25 11:13:41 -0400

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,38 +1,39 @@
+const ultimaAtualizacao = "25/07/2025"; // SEMPRE ATUALIZAR
 const dadosEmpresas = [
     {
         nome: "CASTELÃO LOCAÇÕES",
         depositos: [
-            { data: "2025-07-15", valor: 1000 }
+            { data: "15-07-2025", valor: 1000 }
         ],
         gastos: [
-            { data: "2025-07-25", valor: 670.81, descricao: "Campanhas" }
+            { data: "25-07-2025", valor: 670.81, descricao: "Campanhas" }
         ]
     },
     {
         nome: "SUPERMERCADO CASTELÃO",
         depositos: [
-            { data: "2025-07-07", valor: 1000 }
+            { data: "07-07-2025", valor: 1000 }
         ],
         gastos: [
-            { data: "2025-07-25", valor: 933.65, descricao: "Campanhas Ads" }
+            { data: "25-07-2025", valor: 933.65, descricao: "Campanhas Ads" }
         ]
     },
     {
         nome: "NORTÃO QUITANDA",
         depositos: [
-            { data: "2025-07-07", valor: 1000 }
+            { data: "07-07-2025", valor: 1000 }
         ],
         gastos: [
-            { data: "2025-07-08", valor: 578.07, descricao: "Campanhas Ads" }
+            { data: "25-07-2025", valor: 578.07, descricao: "Campanhas Ads" }
         ]
     },
     {
         nome: "VARANDA ESPETARIA",
         depositos: [
-            { data: "2025-07-15", valor: 1000 }
+            { data: "15-07-2025", valor: 1000 }
         ],
         gastos: [
-            { data: "2025-07-11", valor: 560.91, descricao: "Campanhas Ads" }
+            { data: "25-07-2025", valor: 560.91, descricao: "Campanhas Ads" }
         ]
     }
 ];
