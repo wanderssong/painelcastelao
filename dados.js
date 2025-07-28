3338f9f319b11fd99a7ee9688b1223dbdaf088f3
sr_wandersson <161793765+wanderssong@users.noreply.github.com>
2025-07-28 08:57:47 -0400

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,12 +1,14 @@
-const ultimaAtualizacao = "25/07/2025"; // SEMPRE ATUALIZAR
+const ultimaAtualizacao = "28/07/2025"; // SEMPRE ATUALIZAR
 const dadosEmpresas = [
     {
         nome: "CASTELÃO LOCAÇÕES",
         depositos: [
-            { data: "15-07-2025", valor: 1000 }
+            { data: "15-07-2025", valor: 1000 },
+            
         ],
         gastos: [
-            { data: "25-07-2025", valor: 670.81, descricao: "Campanhas" }
+            { data: "25-07-2025", valor: 670.81, descricao: "Campanhas" },
+            { data: "28-07-2025", valor: 100.00, descricao: "Campanhas" }
         ]
     },
     {
@@ -16,7 +18,8 @@ const dadosEmpresas = [
             { data: "25-07-2025", valor: 1250 },
         ],
         gastos: [
-            { data: "25-07-2025", valor: 933.65, descricao: "Campanhas Ads" }
+            { data: "25-07-2025", valor: 933.65, descricao: "Campanhas Ads" },
+            { data: "28-07-2025", valor: 120.00, descricao: "Campanhas Ads" }
         ]
     },
     {
@@ -25,7 +28,8 @@ const dadosEmpresas = [
             { data: "07-07-2025", valor: 1000 }
         ],
         gastos: [
-            { data: "25-07-2025", valor: 578.07, descricao: "Campanhas Ads" }
+            { data: "25-07-2025", valor: 578.07, descricao: "Campanhas Ads" },
+            { data: "28-07-2025", valor: 140.00, descricao: "Campanhas Ads" }
         ]
     },
     {
@@ -34,7 +38,8 @@ const dadosEmpresas = [
             { data: "15-07-2025", valor: 1000 }
         ],
         gastos: [
-            { data: "25-07-2025", valor: 560.91, descricao: "Campanhas Ads" }
+            { data: "25-07-2025", valor: 560.91, descricao: "Campanhas Ads" },
+            { data: "28-07-2025", valor: 115.00, descricao: "Campanhas Ads" }
         ]
     }
 ];
