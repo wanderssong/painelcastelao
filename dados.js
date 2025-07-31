6b8829b5a18d043239c0c08fc271eadc5febd25e
sr_wandersson <161793765+wanderssong@users.noreply.github.com>
2025-07-31 10:29:13 -0400

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,4 +1,4 @@
-const ultimaAtualizacao = "30/07/2025"; // SEMPRE ATUALIZAR
+const ultimaAtualizacao = "31/07/2025"; // SEMPRE ATUALIZAR
 const dadosEmpresas = [
     {
         nome: "CASTELÃO LOCAÇÕES",
@@ -10,6 +10,7 @@ const dadosEmpresas = [
             { data: "25-07-2025", valor: 670.81, descricao: "Campanhas" },
             { data: "28-07-2025", valor: 100.00, descricao: "Campanhas" },
             { data: "30-07-2025", valor: 100.00, descricao: "Campanhas" },
+            { data: "31-07-2025", valor: 80.00, descricao: "Final de semana" }
         ]
     },
     {
@@ -21,20 +22,22 @@ const dadosEmpresas = [
         gastos: [
             { data: "25-07-2025", valor: 933.65, descricao: "Campanhas Ads" },
             { data: "28-07-2025", valor: 120.00, descricao: "Campanhas Padaria" },
-            { data: "30-07-2025", valor: 90.00, descricao: "Campanhas Pais" }
+            { data: "30-07-2025", valor: 90.00, descricao: "Campanhas Pais" },
+            { data: "31-07-2025", valor: 120.00, descricao: "Sorteio pais" },
         ]
     },
     {
         nome: "NORTÃO QUITANDA",
         depositos: [
             { data: "07-07-2025", valor: 1000 },
-            { data: "07-07-2025", valor: 1200 }
+            { data: "30-07-2025", valor: 1200 }
 
         ],
         gastos: [
             { data: "25-07-2025", valor: 578.07, descricao: "Campanhas Ads" },
             { data: "28-07-2025", valor: 140.00, descricao: "Campanhas Ads" },
-            { data: "30-07-2025", valor: 156.00, descricao: "Campanhas Ads" }
+            { data: "30-07-2025", valor: 156.00, descricao: "Campanhas Ads" },
+            { data: "31-07-2025", valor: 98.30, descricao: "Quinta verde" },
         ]
     },
     {
@@ -47,6 +50,7 @@ const dadosEmpresas = [
             { data: "25-07-2025", valor: 560.91, descricao: "Campanhas Ads" },
             { data: "28-07-2025", valor: 115.00, descricao: "Campanhas Ads" },
             { data: "30-07-2025", valor: 95.00, descricao: "Campanhas Ads" },
+            { data: "31-07-2025", valor: 80.00, descricao: "Campanhas Ads" },
         ]
     }
 ];
