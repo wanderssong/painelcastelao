f989e9da02897f2939e18402debae2c08761569c
sr_wandersson <161793765+wanderssong@users.noreply.github.com>
2025-08-19 10:10:34 -0400

    Update dados.js

--- dados.js
+++ dados.js
@@ -68,7 +68,7 @@ const dadosEmpresas = [
             { data: "08-08-2025", valor: 65.00, descricao: "Dia dos Pais" },
             { data: "12-08-2025", valor: 95.00, descricao: "Atacado" },
             { data: "19-08-2025", valor: 100.00, descricao: "Oferta semana" },
-            { data: "19-08-2025", valor: 50.00, descricao: "Agua de coco" },
+            { data: "19-08-2025", valor: 80.00, descricao: "Agua de coco" },
         ]
     },
     {
